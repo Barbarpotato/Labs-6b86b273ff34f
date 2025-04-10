e29446a02ad26afb5748e7b62c8cc708e56e5dd1
Barbarpotato <darmawanjr88@gmail.com>
2025-04-10 21:50:45 +0800

    check

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    siteUrl: 'https://barbarpotato.github.io/Labs/',
+    siteUrl: 'https://barbarpotato.github.io/Labs-6b86b273ff34f/',
     generateSitemap: true,
     output: 'export',
-    basePath: '/Labs', // The repository name
-    assetPrefix: '/Labs', // The repository name
+    basePath: '/Labs-6b86b273ff34f', // The repository name
+    assetPrefix: '/Labs-6b86b273ff34f', // The repository name
 }
 
 export default nextConfig
